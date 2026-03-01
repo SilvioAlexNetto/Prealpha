ebb66f986d55f48163f8c9ff2042425e4fb33c52
silvio-a-netto <silvio.alex.netto@hotmail.com>
2026-03-01 07:59:07 -0300

    fix FRONTEND folder tracking

--- frontend/src/pages/Cardapio.jsx
+++ frontend/src/pages/Cardapio.jsx
@@ -39,7 +39,7 @@ export default function Cardapio() {
     async function gerarCardapio() {
         setCarregando(true);
         try {
-            const res = await fetch("${BASE_URL}/cardapio", {
+            const res = await fetch("https://prealpha.onrender.com/cardapio", {
                 method: "POST"
             });
             const data = await res.json();
