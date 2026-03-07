ac926d23c2f748d5320a7033208ee620dc381476
silvio-a-netto <silvio.alex.netto@hotmail.com>
2026-03-07 20:22:26 -0300

    ajustando erros do estilo global 1

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import "./styles/global.css";
+import './index.css'
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
@@ -10,11 +11,11 @@ createRoot(document.getElementById('root')).render(
   </StrictMode>,
 )
 
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/service-worker.js")
-      .then(() => console.log("Service Worker registrado"))
-      .catch(err => console.error("Erro no SW:", err));
-  });
-}
+//if ("serviceWorker" in navigator) {
+//window.addEventListener("load", () => {
+//navigator.serviceWorker
+//.register("/service-worker.js")
+//.then(() => console.log("Service Worker registrado"))
+//.catch(err => console.error("Erro no SW:", err));
+//});
+//}
