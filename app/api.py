9945b2201149fa8bad6cf16d78ae9c24e6fdb62c
silvio-a-netto <silvio.alex.netto@hotmail.com>
2026-03-22 22:13:46 -0300

    pfv funciona

--- app/api.py
+++ app/api.py
@@ -29,6 +29,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost",
         "https://localhost",
+        "https://prealpha.onrender.com/",
         "capacitor://localhost",
         "ionic://localhost"
     ],
